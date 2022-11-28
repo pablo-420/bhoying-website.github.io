3eb43a5961394ce51abe294b51605e3e54c3df8f
pablo420 <62798150+pablo-420@users.noreply.github.com>
2022-11-28 18:28:36 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing",{
+  strings:["", "Bhoying Baetiong", "", "Tattoo Artist"],
+  typeSpeed:100,
+  BackSpeed:30,
+  loop:true
+})
